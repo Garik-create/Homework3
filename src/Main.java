e35dd48c28ef31ec6b37875166f36cfdf925fdc1
Garik-create <pfdfkbybujhm@yandex.ru>
2022-08-07 08:19:13 +0300

    Исправил замечания.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         //задача 1
-        byte clientOS = 1;
+        byte clientOS = 0;
         switch (clientOS) {
             case 1:
                 System.out.println("Установите версию приложения для Android по ссылке");
@@ -18,32 +18,29 @@ public class Main {
 
         //задача 2
         int clientDeviceYear = 2014;
-        boolean isIssueYear = clientDeviceYear >= 2015;
-        if (clientOS == 0 && isIssueYear) {
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-            else if (clientOS == 0 && isIssueYear == false) {
-                System.out.println("Установите облегченную версию приложения для для iOS по ссылке");
-            }
-            else if (clientOS == 1 && isIssueYear) {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
-            else {
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
 
-            //задача 3
+        //задача 3
         int year = 2100;
-            if (year % 4 == 0 && year % 100 != 0) {
-                System.out.println(year + " год является високосным.");
-            } else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
-                System.out.println(year + " год является високосным.");
-            }else {
-                System.out.println(year + " год не является високосным.");
-            }
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным.");
+        } else {
+            System.out.println(year + " год не является високосным.");
+        }
 
-            // задача 4
-        int deliveryDistance = 95;
+        // задача 4
+        int deliveryDistance = -15;
+        if (deliveryDistance < 0) {
+            System.out.println("Не верно введено расстояние. Не может быть отрицательным числом.");
+        } else {
             int deliveryDays = 1;
             int distantDifference = deliveryDistance - 20;
             if (distantDifference < 0) {
@@ -53,66 +50,67 @@ public class Main {
                 System.out.println("Потребуется дней: " + deliveryRatio);
 
             }
+        }
 
-            //задание 5
+        //задание 5
         int monthNumber = 12;
-            switch (monthNumber) {
-                case 1:
-                case 2:
-                case 12:
-                    System.out.println("Зима");
-                    break;
-                case 3:
-                case 4:
-                case 5:
-                    System.out.println("Весна");
-                    break;
-                case 6:
-                case 7:
-                case 8:
-                    System.out.println("Лето");
-                    break;
-                case 9:
-                case 10:
-                case 11:
-                    System.out.println("Осень");
-                    break;
-                default:
-                    System.out.println("Номер месяца больше 12");
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Номер месяца больше 12");
 
-            }
-            // задание 6
+        }
+        // задание 6
         int age = 19;
-            int salary = 58_000;
-            int ageElderLimit = 3 * salary;
-            int ageYongerLimit = 2 * salary;
-            double overSalaryLimit = 1.2;
-            double overSalaryLimit2 = 1.5;
+        int salary = 58_000;
+        int ageElderLimit = 3 * salary;
+        int ageYongerLimit = 2 * salary;
+        double overSalaryLimit = 1.2;
+        double overSalaryLimit2 = 1.5;
 
-            if (age >= 23 && salary < 50_000) {
-                System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + ageElderLimit + " рублей");
-            } else if (age >= 23 && salary >= 50_000 && salary < 80_000) {
-                System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (int) (ageElderLimit * overSalaryLimit) + " рублей");
-            } else if (age >= 23 && salary >= 80_000) {
-                System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (int) (ageElderLimit * overSalaryLimit2) + " рублей");
-            } else if (age < 23 && salary < 50_000) {
-                System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + ageYongerLimit + " рублей");
-            } else if (age < 23 && salary >= 50_000 && salary < 80_000) {
-                System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (int) (ageYongerLimit * overSalaryLimit) + " рублей");
-            } else {
-                System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (int) (ageYongerLimit * overSalaryLimit2) + " рублей");
-            }
+        if (age >= 23 && salary < 50_000) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + ageElderLimit + " рублей");
+        } else if (age >= 23 && salary >= 50_000 && salary < 80_000) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (int) (ageElderLimit * overSalaryLimit) + " рублей");
+        } else if (age >= 23 && salary >= 80_000) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (int) (ageElderLimit * overSalaryLimit2) + " рублей");
+        } else if (age < 23 && salary < 50_000) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + ageYongerLimit + " рублей");
+        } else if (age < 23 && salary >= 50_000 && salary < 80_000) {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (int) (ageYongerLimit * overSalaryLimit) + " рублей");
+        } else {
+            System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (int) (ageYongerLimit * overSalaryLimit2) + " рублей");
+        }
 
-            // задание 7
+        // задание 7
         int age1 = 25;
-            double salary2 = 60_000;
-            double wantedSum = 330_000;
-            double basePercent = 10;
-            boolean isMoreThan30 = (age1 >= 30);
-            boolean isLessThan23 = (age1 < 23);
-            boolean isBeetween23_30 = (age1 >= 23 && age1 < 30);
-            boolean isSalaryMore80 = (salary2 > 80_000);
-            double maxDuty = 0.5 * salary2;
+        double salary2 = 60_000;
+        double wantedSum = 330_000;
+        double basePercent = 10;
+        boolean isMoreThan30 = (age1 >= 30);
+        boolean isLessThan23 = (age1 < 23);
+        boolean isBeetween23_30 = (age1 >= 23 && age1 < 30);
+        boolean isSalaryMore80 = (salary2 > 80_000);
+        double maxDuty = 0.5 * salary2;
         double creditPayment;
         double dutyMore80 = basePercent - 0.7;
         double dutyLess23 = basePercent + 1;
